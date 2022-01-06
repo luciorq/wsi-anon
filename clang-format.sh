de112761ae8f976659f681428766b80358cde263
mfranz <michael.franz@charite.de>
2022-01-06 12:22:31 +0100

    fixed code check

--- clang-format.sh
+++ clang-format.sh
@@ -8,9 +8,7 @@ echo
 
 find . \( \( -name \*.c -o -name \*.h \) -a ! -iname \*soap\* \) -print0 | xargs -0 -n 1 clang-format-10 -i --verbose
 
-modifiedFiles==`git status --porcelain | grep '^ M' | cut -c4-`
-echo "MODIFIED FILES"
-echo $modifiedFiles
+modifiedFiles=`git status --porcelain | grep '^ M' | cut -c4-`
 
 if [[ -z $modifiedFiles ]]; then
   echo "Formatting passed!"
@@ -18,10 +16,10 @@ if [[ -z $modifiedFiles ]]; then
 else
   echo "The following files have clang-format problems:"
   git diff --stat $modifiedFiles
-  echo "Please run"
+  echo "Please run code check before committing: "
   echo
   echo "find . \( \( -name \*.c -o -name \*.h \) -a ! -iname \*soap\* \) -print0 | xargs -0 -n 1 clang-format-10 --Werror -i --verbose"
-  echo
-  echo "to solve the issue."
   git reset HEAD --hard
 fi
+
+exit 1
