b9b38930689efb71e288927ed32ed76e5bf805d3
mfranz <michael.franz@charite.de>
2022-01-10 09:25:02 +0100

    fixed jpeg soi header

--- src/defines.h
+++ src/defines.h
@@ -55,7 +55,7 @@ typedef enum {
 #define DOUBLE 12
 #define LONG8 16
 
-#define JPEG_SOI "\xff\xd8\0"
+#define JPEG_SOI "\xff\xd8\xff\xe0\0"
 #define JPEG_EOI "\xff\xd9\0"
 #define LZW_CLEARCODE "\x80\0"
 
