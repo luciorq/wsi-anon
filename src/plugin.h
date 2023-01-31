982118e6e2c35e6ce3d0d082b57d553db88fc77e
Isil Dogan O <isil.dogan-o@charite.de>
2023-01-31 10:22:57 +0100

    fix comments

--- src/plugin.h
+++ src/plugin.h
@@ -1,10 +1,9 @@
 /*
  *
- * Include this header file in your source file in order to execute anonymization on an additional
- * format. Add your format to the 'VENDOR_STRINGS' variable in wsi-anonymizer.h right before the
- * 'Unknown' entry. Implement the following functions declared below. Add is_format() and
- * handle_format() to the *is_format_functions[] and *handle_format_functions[] arrays in
- * wsi-anonymizer.c.
+ * In order to execute anonymization on an additional format, include this header file in your
+ * source file. Add your format to the 'VENDOR_STRINGS' variable in wsi-anonymizer.h right before
+ * the 'Unknown' entry. Implement the functions declared below. Add is_format() and handle_format()
+ * to the *is_format_functions[] and *handle_format_functions[] arrays in wsi-anonymizer.c.
  *
  */
 
@@ -14,6 +13,6 @@
 // checks if the file format at hand is actually the expected file format
 int32_t is_format(const char *filename);
 
-// implements the anonymization that is called within the handle_<YOUR_FORMAT>() function
+// implements the anonymization for the added format
 int32_t handle_format(const char **filename, const char *new_label_name, bool keep_macro_image,
                       bool disable_unlinking, bool do_inplace);
