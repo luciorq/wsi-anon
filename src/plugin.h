89bfec058f654d4eef6365aa7cddd624b6bfd0b9
Isil Dogan O <isil.dogan-o@charite.de>
2023-02-02 13:56:55 +0100

    rework comment in plugin.h

--- src/plugin.h
+++ src/plugin.h
@@ -1,9 +1,14 @@
 /*
  *
- * In order to execute anonymization on an additional format, include this header file in your
- * source file. Add your format to the 'VENDOR_STRINGS' variable in wsi-anonymizer.h right before
- * the 'Unknown' entry. Implement the functions declared below. Add is_format() and handle_format()
- * to the *is_format_functions[] and *handle_format_functions[] arrays in wsi-anonymizer.c.
+ * In order to execute anonymization on an additional format, include this header file in the header
+ * file of your new format. For conventional purposes name the newly created header and source file
+ * of your format <YOUR_FORMAT>-io.h and <YOUR_FORMAT>-io.c. Implement the functions declared below.
+ * You can either keep these functionnames or use them as a template and change them to
+ * is_<YOUR_FORMAT>() and handle_<YOUR_FORMAT>() (this is recommenended when you want to implement
+ * the anonymiaztion of more than one format). Add your format to the 'VENDOR_STRINGS' variable in
+ * wsi-anonymizer.h right before the 'Unknown' entry. Lastly, add &is_format() and &handle_format()
+ * (or &is_<YOUR_FORMAT>() and &handle_<YOUR_FORMAT>()) to the *is_format_functions[] and
+ * *handle_format_functions[] arrays in wsi-anonymizer.c.
  *
  */
 
