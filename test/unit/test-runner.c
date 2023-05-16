8dac4e4b9953b9d9dd3ab05208cdf1a7bba2a26f
Michael Franz <michael.franz.sy@gmail.com>
2023-05-16 13:52:23 +0200

    reworked test runner

--- test/unit/test-runner.c
+++ test/unit/test-runner.c
@@ -22,21 +22,16 @@ int main() {
         CU_set_output_filename("Test-Wsi-Anon");
         
         fprintf(stdout, "Running tests...\n");
+        CU_automated_enable_junit_xml(CU_TRUE);
         CU_automated_run_tests();
 
-        fprintf(stdout, "_______________________________________________\n");
-        unsigned int num_run = CU_get_number_of_tests_run();
-        unsigned int num_inactive = CU_get_number_of_tests_inactive();
-        unsigned int num_fails = CU_get_number_of_tests_failed();
-        fprintf(stdout, "Result: %u succeeded - %u skipped - %u failed\n", num_run, num_inactive,
-                num_fails);
 
-        CU_pFailureRecord records = CU_get_failure_list();
-        while (records++ != NULL) {
-            fprintf(stdout, "\n* Error in test %s\n", records->pTest->pName);
-        }
+        fprintf(stdout, "_______________________________________________\n");
+        CU_pRunSummary summary = CU_get_run_summary();
+        fprintf(stdout, "Result: %u succeeded - %u skipped - %u failed\n", summary->nTestsRun, summary->nTestsInactive,
+                summary->nTestsFailed);
         
-        result = (num_fails != 0);
+        result = (summary->nTestsFailed != 0);
         CU_cleanup_registry();
     }
     return result;
