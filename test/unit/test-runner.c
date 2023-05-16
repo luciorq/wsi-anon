67bcd13598406615d0e7c2d49d93ed2a94eaad3b
Michael Franz <michael.franz.sy@gmail.com>
2023-05-16 15:21:54 +0200

    removed fprintf in test runner

--- test/unit/test-runner.c
+++ test/unit/test-runner.c
@@ -10,23 +10,23 @@
 int main() {
     int result = 1;
     if (CU_initialize_registry()) {
-        fprintf(stderr, "Initialization of Test Registry failed.\n");
+        printf("Initialization of Test Registry failed.\n");
         exit(result);
     } else {
-        fprintf(stdout, "Initializing tests\n");
+        printf("Initializing tests\n");
         AddTestsUtils();
         AddTestsIniParser();
         AddTestsWsiAnonymizer();
 
-        fprintf(stdout, "Set output filename to Test-Wsi-Anon\n");
+        printf("Set output filename to Test-Wsi-Anon\n");
         CU_set_output_filename("Test-Wsi-Anon");
 
-        fprintf(stdout, "Running tests...\n");
+        printf("Running tests...\n");
         CU_automated_run_tests();
 
-        fprintf(stdout, "_______________________________________________\n");
+        printf("_______________________________________________\n");
         CU_pRunSummary summary = CU_get_run_summary();
-        fprintf(stdout, "Result: %u succeeded - %u skipped - %u failed\n", summary->nTestsRun,
+        printf("Result: %u succeeded - %u skipped - %u failed\n", summary->nTestsRun,
                 summary->nTestsInactive, summary->nTestsFailed);
 
         //result = (summary->nTestsFailed != 0);
