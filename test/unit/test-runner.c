f03d9ab2613b285239410f1aa3bdcfb45e23dfbf
Michael Franz <michael.franz.sy@gmail.com>
2023-05-16 15:12:38 +0200

    test

--- test/unit/test-runner.c
+++ test/unit/test-runner.c
@@ -10,18 +10,12 @@
 int main() {
     int result = 1;
     if (CU_initialize_registry()) {
-        fprintf(stderr, "Initialization of Test Registry failed.\n");
         exit(result);
     } else {
-        fprintf(stdout, "Initializing tests\n");
         AddTestsUtils();
         AddTestsIniParser();
         AddTestsWsiAnonymizer();
-
-        fprintf(stdout, "Set output filename to Test-Wsi-Anon\n");
         CU_set_output_filename("Test-Wsi-Anon");
-
-        fprintf(stdout, "Running tests...\n");
         CU_automated_run_tests();
 
         unsigned int num_fails = CU_get_number_of_failures();
