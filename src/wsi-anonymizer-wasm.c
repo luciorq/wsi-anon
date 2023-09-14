2d2176f38c21d220c488a3633389efa974f3540b
Isil Dogan O <isil.dogan-o@charite.de>
2023-09-14 11:31:41 +0200

    fix wasm target build

--- src/wsi-anonymizer-wasm.c
+++ src/wsi-anonymizer-wasm.c
@@ -2,8 +2,7 @@
 
 #include <emscripten.h>
 
-int32_t EMSCRIPTEN_KEEPALIVE wsi_anonymize(const char *filename, const char *new_filename,
-                                           const char *pseudonym_metadata, bool keep_macro_image,
+int32_t EMSCRIPTEN_KEEPALIVE wsi_anonymize(const char *filename, const char *new_label_name, bool keep_macro_image,
                                            bool disbale_unlinking) {
-    return anonymize_wsi_inplace(filename, new_filename, pseudonym_metadata, keep_macro_image, disbale_unlinking);
+    return anonymize_wsi_inplace(filename, new_label_name, keep_macro_image, disbale_unlinking);
 }
